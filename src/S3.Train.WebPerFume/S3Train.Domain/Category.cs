8fc21eab94a2e016c6397f9e6844f833e20e727a
traxuansongithub <46294221+traxuansongithub@users.noreply.github.com>
2020-02-21 14:31:18 +0700

    Init Architecture Project (Domain - Service - FrameWork - Web)

---
+++ src/S3.Train.WebPerFume/S3Train.Domain/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3Train.Domain
+{
+   public class Category : EntityBase
+    {
+        [ForeignKey("Category")]
+        public Guid? ParentId { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public virtual Category ParentCategory { get; set; }
+        public virtual ICollection<Category> ChildCategories { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
